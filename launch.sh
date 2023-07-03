266728f97d7945f909da4473a418530ec81a44fd
Snowflake Nightingale <freeze-dolphin@outlook.com>
2023-07-03 02:55:48 +0000

    remote control & update

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ ls -a
 
 sed -i 's/{{PASSWD}}/'"$PASSWD"'/g' MinecraftClient.ini
 
-chmod a+x MinecraftClient-20230614-223-linux-x64
+chmod a+x MinecraftClient-20230623-225-linux-x64
 
-./MinecraftClient-20230614-223-linux-x64 MinecraftClient.ini
+./MinecraftClient-20230623-225-linux-x64 MinecraftClient.ini
