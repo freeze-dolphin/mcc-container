cf816d5a23e9eaf5013df12551243b96dcc9180a
Sapphira Tempest <freeze-dolphin@outlook.com>
2023-11-25 07:41:07 +0000

    feat: upgrade to 1.20.2

--- launch.sh
+++ launch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+mcc_exec="MinecraftClient-20231117-76-linux-x64"
+
 ls -a
 
 sed -i 's/{{PASSWD}}/'"$PASSWD"'/g' MinecraftClient.ini
 sed -i 's/{{BOT_TOKEN}}/'"$BOT_TOKEN"'/g' MinecraftClient.ini
 
-chmod a+x MinecraftClient-20231120-234-linux-x64
+chmod a+x ${mcc_exec}
 
 while true
 do
-    ./MinecraftClient-20230623-225-linux-x64 MinecraftClient.ini
+    ./${mcc_exec} MinecraftClient.ini
 done
